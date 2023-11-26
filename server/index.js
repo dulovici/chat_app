8feb39525d6f1ba21ff4101c00191dbeeccde25a
Igor Dulovic <igorx24@gmail.com>
2023-11-26 09:45:20 +0100

    server init

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+server.listen(3001, ()=> {
+    console.log("SERVER RUNNING")
+})
